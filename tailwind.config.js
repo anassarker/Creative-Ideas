080cd1cb63c988879f3f405d3c0e66185d28e848
anassarker <anassarker500@gmail.com>
2023-09-28 16:24:59 +0600

    front page with navbar and design added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens:{
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    container:{
+      center: true,
+      padding: '1rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
